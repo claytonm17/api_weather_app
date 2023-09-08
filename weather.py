5c1e96823bccbe415853904c4d99066934fe4169
claytonm17 <you@example.com>
2023-09-08 19:32:24 -0400

    Create basic weather program within command line File containing API key hidden for privacy

--- weather.py
+++ weather.py
@@ -1,1 +1,19 @@
-print('Hello World')
+import requests
+from api_key import serviceurl, api_key
+
+#Initial input and request
+city = input('Enter city: ')
+if len(city) < 1: city = 'Columbus'  #For testing
+url = serviceurl + 'appid=' + api_key + "&q=" + city #hiding API key
+data = requests.get(url)
+
+#Data pulled from API
+weather = data.json()['weather'][0]['main']
+temp = data.json()['main']['temp']
+
+#Converting temp to fahrenheit
+tempF = round(temp*(9/5) - 459.67)
+
+#Output
+print('The weather in', city, 'is', weather)
+print('The temperature in', city, 'is', tempF,'F')
